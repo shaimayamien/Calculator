febbd79ad3384becaf19897c2884e91dc76e5813
HP <HP@192.168.1.7>
2020-02-25 20:02:11 +0200

    Calculator

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,26 @@
+package Calculator;
+import  java.util.Scanner;
+
+public class Calculator{
+    public Calculator () 
+  {
+	  
+  }
+    public int add (int a, int b) 
+  	{
+ 	return a+b;
+ 	}
+  
+   public static void main(String args[])
+   {
+       Calculator myCalculator = new Calculator();
+  
+     Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int a = input.nextInt();
+        System.out.println("Enter a number: ");
+        int b = input.nextInt();
+        System.out.println(myCalculator.add(a,b));
+   }
+
+}
